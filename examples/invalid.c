d07c3d3e6d1565703882f969410c583a808e1dcd
C-Compiler Project <project@c-compiler.local>
2026-09-16 19:45:32 +0530

    test: add compiler example programs

---
+++ examples/invalid.c
@@ -0,0 +1,8 @@
+int main() {
+    float x = 3.14;
+    int y;
+
+    y = x;
+
+    return 0;
+}
---
+++ examples/mytest.c
@@ -0,0 +1,13 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x = 10;
+    int y = 20;
+    int result = add(x, y);
+
+    printf("Result = %d\n", result);
+
+    return 0;
+}
